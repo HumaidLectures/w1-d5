46426e4c1307d15970a642adf7d70b36d53dc46c
HumaidLectures <92582816+HumaidLectures@users.noreply.github.com>
2021-10-15 15:12:25 +0300

    Add files via upload

---
+++ w1-d5/src/day5.kt
@@ -0,0 +1,71 @@
+fun main() {
+//    val names = listOf("ahmed" , "khaled" , "rakan")
+//    val x = names.getOrElse(3){
+//        "index $it is out of range"
+//    }
+//    println(x)
+//
+//    val numbers = listOf(22,18,25)
+//    val y =numbers.getOrNull(3)
+//    println(y)
+//
+//    val nums = mutableListOf(15,90,10)
+//    println(nums)
+//    nums.add(1,99)
+//    println(nums)
+
+
+//    val names = mutableListOf("anas" , "khaled" , "ahmed")
+//    names.add(1,"rakan")
+//    println(names)
+//    names.add(2,"Rakan")
+//    println(names)
+
+//
+//    val doubles = mutableListOf(10.5 , 5.9 , 4.8)
+//    doubles.set(2,99.4)
+//    println(doubles)
+//    doubles[2] = 99.5
+//    println(doubles)
+
+//    val cars = mutableListOf("car1" , "car2" , "car3" , "car4")
+//    println(cars)
+//    cars.remove("car3")
+//    println(cars)
+//    cars.removeAt(2)
+//    println(cars)
+//    cars -= "car1"
+//    println(cars)
+
+//
+//    val stations = mutableListOf("water" , "food" , "juice" , "water")
+//
+//    stations.forEachIndexed { index, s ->
+//        stations[index] += " is"
+//    }
+//    println(stations)
+//    stations.forEach {
+//        println(it)
+//    }
+//
+//    val ids = setOf(100,200,300,200,300)
+//
+//    println(ids)
+//    println(ids.contains(100))
+//
+//    val ids1 = mutableSetOf(1,2,3)
+//    println(ids1)
+//    ids1.add(4)
+//    println(ids1)
+//    ids1.remove(4)
+//    println(ids1)
+
+//    val namess = mutableListOf(1,2,3)
+//    namess.toSet()
+//
+    val x = mutableMapOf(111111 to "ahmed")
+    println(x)
+    x[1456121] = "khaled"
+    println(x)
+
+}
